80ee19e20b1242bed0e2f77c1fd3ab32c7b82d28
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-02-10 14:04:05 +0300

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" main file for testing """
+""" 
+main file for testing 
+"""
 
 def minOperations(n):
-    """ calculates fewest number of operations"""
-    if n <= 1:
-        return 0
-        Args, evn, Operations = n, 2, 0
-
-    while Args > 1:
-        if Args % evn == 0:
-            Args = Args / evn
-            else:
-                evn += 1
-    return Opertaions
+    """
+    calculates fewest number of operations
+    """
+    a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+            b += 1
+            return a
