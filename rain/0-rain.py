a79e612ac1b0a111ca68fb9e3a5e94afc8e70eb7
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-03 21:35:03 +0300

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -6,7 +6,7 @@ calculating square units of water
 def rain(walls):
     """units of water retained"""
     quantity = 0
-    for i in range(1, len(walls) -1):
+    for i in range(1, len(walls) - 1):
         left = walls[i]
         for j in range(i):
             left = max(left, walls[j])
