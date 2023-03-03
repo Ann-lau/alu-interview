aa2b7996395009e51d0e2b9a753968ea2a04e877
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-03 21:30:45 +0300

    rain

---
+++ rain/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+main file
+"""
+rain = __imort__('0-rain').rain
+
+if __name__ = "__main__":
+walls = [0, 1, 0, 2, 0, 3, 0, 4]
+print(rain(walls))
---
+++ rain/0-rain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+calculating square units of water
+"""
+
+def rain(walls):
+    """units of water retained"""
+    quantity = 0
+    for i in range(1, len(walls) -1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+    return quantity
