76dc2922b7f5d1ab9c531e1c5ec5a02164645e78
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-24 16:17:55 +0300

    pascals triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-def printPascal(n):
+"""pPrint Pascals triangle"""
 
-    for i in range(n+1):
-        c=1
-        for j in range(1, i+1):
-            print(c, sep=' ', end='')
-            c=c*(i-j)//j
-        print()
-#driver code
-n = int(input("num: "))
-printPascal(n)
+from math import factorial
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal’s triangle"""
+    if n <= 0:
+        return[]
+    
+    n = 5
+    for i in range(n):
+        for j in range(n-i+1)
+
+        #for left spacing
+        print(end=" ")
+
+    for j in range(i+1):
+
+        #nCr = n!/((n-r)!*r!)
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
