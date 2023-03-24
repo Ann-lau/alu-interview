241e2dc46e757ea020003338b3fd7050d24b0883
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-24 16:40:09 +0300

    pascals triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,11 +3,16 @@
 
 def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal’s triangle"""
-
-    n = 5
-    for i in range (n):
-        # adjust space
-        print(' '*(n-i), end='')
-
-        # compute power of 11
-        print(' '.join(map(str, str(11**i))))
+    if n <= 0:
+        return[]
+    
+    pascal = [1.1]
+    for i in range(n-2):
+        new = []
+        for i in range(len(pascal)-1):
+            left = pascal[i]
+            right = pascal[i + 1]
+            new.append(left + right)
+        new = [1] + new + [1]
+        pascal = new
+    return pascal
