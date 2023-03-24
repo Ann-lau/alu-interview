db4b294047f2fc27b5a70c63c4486c3d83b12a79
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-24 16:44:40 +0300

    pascals triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -5,14 +5,11 @@ def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal’s triangle"""
     if n <= 0:
         return[]
-    
-    pascal = [1.1]
-    for i in range(n-2):
-        new = []
-        for i in range(len(pascal)-1):
-            left = pascal[i]
-            right = pascal[i + 1]
-            new.append(left + right)
-        new = [1] + new + [1]
-        pascal = new
-    return [pascal]
+     pascal = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        row.append(1)
+        pascal.append(row)
+    return pascal
